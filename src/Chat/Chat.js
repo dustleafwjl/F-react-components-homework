cf51c7b75f0ee27ed39368e8b14712b45dd0e2fe
Jianglin Wang <jianglin.wang@Jianglins-MacBook-Pro.local>
2020-09-22 13:44:31 +0800

    [jianglin] feat: finshed

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -5,6 +5,7 @@ import ChatBox from './ChatBox/ChatBox';
 import ChatInput from './ChatInput/ChatInput';
 import shopData from '../data/shop.json';
 import answersData from '../data/answers.json';
+import { ROLE } from '../constants';
 
 class Chat extends Component {
   constructor(props, context) {
@@ -27,13 +28,39 @@ class Chat extends Component {
     }, 1000);
   }
 
+  postMessage = (text) => {
+    const customerData = {
+      tags: [],
+      text,
+      role: ROLE.CUSTOMER,
+    };
+    this.setState(
+      {
+        messages: [...this.state.messages, customerData],
+      },
+      () => {
+        this.answerMessage(text);
+      }
+    );
+  };
+
+  answerMessage = (tag) => {
+    const answerText = answersData.find((answer) => answer.tags.includes(tag));
+    if (!answerText) {
+      return;
+    }
+    this.setState({
+      messages: [...this.state.messages, answerText],
+    });
+  };
+
   render() {
     const { shop, messages } = this.state;
     return (
       <main className="Chat">
         <ChatHeader shop={shop} />
         <ChatBox messages={messages} />
-        <ChatInput />
+        <ChatInput postMessage={this.postMessage} />
       </main>
     );
   }
--- src/Chat/ChatInput/ChatInput.js
+++ src/Chat/ChatInput/ChatInput.js
@@ -2,11 +2,33 @@ import React, { Component } from 'react';
 import './ChatInput.scss';
 
 class ChatInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: '',
+    };
+  }
+
+  handleInputChange = (e) => {
+    this.setState({
+      inputValue: e.target.value,
+    });
+  };
+
+  handleButtonClick = () => {
+    if (this.state.inputValue === '') {
+      return;
+    }
+    this.props.postMessage(this.state.inputValue);
+  };
+
   render() {
     return (
       <footer className="ChatInput">
-        <input type="text" />
-        <button type="button">Send</button>
+        <input value={this.state.inputValue} onChange={this.handleInputChange} type="text" />
+        <button type="button" onClick={this.handleButtonClick}>
+          Send
+        </button>
       </footer>
     );
   }
